c16a5d80f5b79a10e9483222b09d71b459bd9daa
InsomniaDev <adam9190@gmail.com>
2020-11-05 16:57:33 -0700

    Update database init method

--- database/database.go
+++ database/database.go
@@ -1,9 +1,9 @@
 package database
 
 // Init will initialize the database and return a Database with the total depth set
-func Init(layers int) Database {
-	db := Database{
+func Init(layers int) (db Database) {
+	db = Database{
 		TotalDepth: layers,
 	}
-	return db
+	return
 }
