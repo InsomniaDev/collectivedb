c27b6bd3a7fa2aeff0fb661780343f6f1e9a67ea
InsomniaDev <adam9190@gmail.com>
2022-12-07 16:54:42 -0700

    working on ip identification

--- internal/control/helper.go
+++ internal/control/helper.go
@@ -1,9 +1,13 @@
 package control
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"regexp"
+	"strings"
 
 	"github.com/google/uuid"
 )
@@ -35,7 +39,29 @@ func determineIpAddress() string {
 		return localAddr.IP.String()
 	}
 
-	// Check for a configuration value
-	
+	checkIfK8s := func() (bool, string) {
+		readfile, err := os.Open("/etc/resolv.conf")
+		if err != nil {
+			return false, ""
+		}
+		searchRegexp := regexp.MustCompile(`^\s*search\s*(([^\s]+\s*)*)$`)
+		fileScanner := bufio.NewScanner(readfile)
+		fileScanner.Split(bufio.ScanLines)
+		for fileScanner.Scan() {
+
+			match := searchRegexp.FindSubmatch([]byte(fileScanner.Text()))
+			if match != nil && strings.Contains(string(match[1]), "svc.cluster.local") {
+				return true, string(match[1])
+			}
+		}
+		return false, ""
+	}
+
+	if isInK8s, svcValue := checkIfK8s(); isInK8s {
+		fmt.Println(svcValue)
+		// Let's do something with the service value here
+		// Need to get the $HOSTNAME env variable here, then create the connection to it
+	}
+
 	return discoverLocalIp()
 }
