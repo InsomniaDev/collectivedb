dd2cc7143e0ad8d79f04e6525d473cecd3b2d50f
InsomniaDev <adam9190@gmail.com>
2024-06-19 14:37:26 -0600

    update .gitignore

--- deployDocker.sh
+++
@@ -1,8 +0,0 @@
-GOOS=linux GOARCH=amd64 go build .
-
-podman build --platform=linux/amd64 -t collective . 
-podman tag collective 192.168.1.30:30500/collective:latest
-podman push 192.168.1.30:30500/collective:latest --tls-verify=false
-
-COLLECTIVE_CONTAINER=`kubectl get pods | grep collective | awk '{ print $1 }'`
-kubectl delete pod $COLLECTIVE_CONTAINER
